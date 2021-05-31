23de9ee67eb0717ec6cc245e24b28d9702618f46
valentina <valentina.alcayaga@alumnos.ucn.cl>
2021-05-31 00:16:29 -0400

    actualization admin.py

--- app1/admin.py
+++ app1/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from app1.models import *
 
 # Register your models here.
-admin.site.register(Usuario)
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Administrador)
 admin.site.register(Curso)
 admin.site.register(Asignatura)
 
